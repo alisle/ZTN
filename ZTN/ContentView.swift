59abf4e44a8ee1dee649e551a714ed6d22f921db
Alex Lisle <alex.lisle@gmail.com>
2020-04-22 10:04:27 -0500

    Initial Commit

---
+++ ZTN/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ZTN
+//
+//  Created by Alex Lisle on 4/22/20.
+//  Copyright © 2020 Alex Lisle. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
